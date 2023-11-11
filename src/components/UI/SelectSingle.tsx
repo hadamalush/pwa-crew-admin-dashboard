2f6f08dc609c7073d75ed1b7e59e6d35dcfbd834
poncy <poncyman@gmail.com>
2023-11-11 03:40:53 +0100

    refactor: Splitted CardStorage Component - Added SelectSingle component

---
+++ src/components/UI/SelectSingle.tsx
@@ -0,0 +1,48 @@
+import useTheme from "../../hooks/useTheme";
+import Select from "react-select";
+import { ComponentPropsWithoutRef } from "react";
+
+type SelectSingleProps = ComponentPropsWithoutRef<typeof Select>;
+
+const SelectSingle = ({ ...props }: SelectSingleProps) => {
+  const { theme } = useTheme();
+
+  return (
+    <Select
+      {...props}
+      placeholder="Select storage"
+      styles={{
+        control: (base, state) => {
+          state.theme.colors = {
+            ...state.theme.colors,
+            primary: theme === "dark" ? "#2a2d3a" : "#e2e8f0", //option hover
+            primary25: theme === "dark" ? "#0f1015" : "#cbd5e1", //option hover & focus
+            primary50: theme === "dark" ? "#0f1015" : "#cbd5e1", //option hover & !focus
+            neutral0: theme === "dark" ? "white" : "black", // option font color
+            neutral20: theme === "dark" ? "#2f2e33" : "#c9c9c980", // border input
+            neutral30: theme === "dark" ? "#c9c9c980" : "#cac7c7", //border hover input
+            neutral40: theme === "dark" ? "#c9c9c980" : "#2f2e33", // indicator hover
+            neutral60: theme === "dark" ? "#cac7c7" : "#cac7c7", // indicator with focus
+            neutral80: "#6c7293", // font color inside input + indicator with hover
+          };
+          base.cursor = "pointer";
+
+          return {
+            ...base,
+            backgroundColor: `${theme === "dark" && "primaryColor"}`,
+          };
+        },
+        option: (state) => ({ ...state, cursor: "pointer" }),
+        menu: (base) => ({
+          ...base,
+          backgroundColor: `${theme === "dark" && "primaryColor"}`,
+          color: `${theme === "dark" ? "white" : "black"}`,
+          cursor: "pointer",
+        }),
+      }}
+      className="mt-5 w-3/4 text-white"
+    />
+  );
+};
+
+export default SelectSingle;
--- src/components/transitions/Cards/CardStorage.tsx
+++ src/components/transitions/Cards/CardStorage.tsx
@@ -1,12 +1,11 @@
 import Container from "../../UI/Container";
-import { Doughnut } from "react-chartjs-2";
 import Heading from "../../UI/Heading";
-import Select from "react-select";
-import "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
 import { ComponentPropsWithoutRef, useState } from "react";
-import useTheme from "../../../hooks/useTheme";
-import { DUMMY_STORAGETYPE } from "../dummy-items";
 import { cn } from "../../../util/utils";
+import { type DUMMY_STORAGETYPE } from "../dummy-items";
+import SelectSingle from "../../UI/SelectSingle";
+import "chart.js/auto";
 
 type CardStorageProps = {
   dataStorage: DUMMY_STORAGETYPE;
@@ -14,7 +13,6 @@ type CardStorageProps = {
 
 const CardStorage = ({ dataStorage, className, ...props }: CardStorageProps) => {
   const [selectedOption, setSelectedOption] = useState("Cloudinary");
-  const { theme } = useTheme();
   const transformedOption = selectedOption.toLocaleLowerCase();
 
   const data = {
@@ -38,17 +36,16 @@ const CardStorage = ({ dataStorage, className, ...props }: CardStorageProps) => 
     cutout: "65%",
   };
 
-  const optionsSelect = [
+  type OptionType = { label: string; value: string };
+  const optionsSelect: OptionType[] = [
     { value: "cloudinary", label: "Cloudinary" },
     { value: "vercelblob", label: "VercelBlob" },
     { value: "mega", label: "Mega" },
   ];
 
-  type OptionType = { label: string; value: string };
-
-  const changeSelectHandler = (values: OptionType | null) => {
-    if (values) {
-      const storage = (values as OptionType).label;
+  const changeSelectHandler = (newValue: unknown | null) => {
+    if (newValue) {
+      const storage = (newValue as OptionType).label;
       setSelectedOption(storage);
     }
   };
@@ -72,42 +69,7 @@ const CardStorage = ({ dataStorage, className, ...props }: CardStorageProps) => 
         </Heading>
         <Doughnut data={data} options={options} />
       </div>
-
-      <Select
-        options={optionsSelect}
-        placeholder="Select storage"
-        onChange={changeSelectHandler}
-        styles={{
-          control: (base, state) => {
-            state.theme.colors = {
-              ...state.theme.colors,
-              primary: theme === "dark" ? "#2a2d3a" : "#e2e8f0", //option hover
-              primary25: theme === "dark" ? "#0f1015" : "#cbd5e1", //option hover & focus
-              primary50: theme === "dark" ? "#0f1015" : "#cbd5e1", //option hover & !focus
-              neutral0: theme === "dark" ? "white" : "black", // option font color
-              neutral20: theme === "dark" ? "#2f2e33" : "#c9c9c980", // border input
-              neutral30: theme === "dark" ? "#c9c9c980" : "#cac7c7", //border hover input
-              neutral40: theme === "dark" ? "#c9c9c980" : "#2f2e33", // indicator hover
-              neutral60: theme === "dark" ? "#cac7c7" : "#cac7c7", // indicator with focus
-              neutral80: "#6c7293", // font color inside input + indicator with hover
-            };
-            base.cursor = "pointer";
-
-            return {
-              ...base,
-              backgroundColor: `${theme === "dark" && "primaryColor"}`,
-            };
-          },
-          option: (state) => ({ ...state, cursor: "pointer" }),
-          menu: (base) => ({
-            ...base,
-            backgroundColor: `${theme === "dark" && "primaryColor"}`,
-            color: `${theme === "dark" ? "white" : "black"}`,
-            cursor: "pointer",
-          }),
-        }}
-        className="mt-5 w-3/4 text-white"
-      />
+      <SelectSingle options={optionsSelect} onChange={changeSelectHandler} />
     </Container>
   );
 };
