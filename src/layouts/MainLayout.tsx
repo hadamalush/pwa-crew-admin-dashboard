9ce70ab59a74e86d794cb928700af09e1a72fa57
poncy <poncyman@gmail.com>
2023-11-27 04:41:51 +0100

    test: style

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "../components/Common/Header";
-// import Navbar from "../components/Common/Navigation/Navbar";
+import Navbar from "../components/Common/Navigation/Navbar";
 import Footer from "../components/Common/Footer";
-// import { MainNavbarItems } from "../components/Common/Navigation/NavigationData";
+import { MainNavbarItems } from "../components/Common/Navigation/NavigationData";
 import { Suspense } from "react";
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-      {/* <Navbar data={MainNavbarItems} id="mainNav" /> */}
+      <Navbar data={MainNavbarItems} id="mainNav" />
       <Suspense fallback={<div className="dark:bg-black bg-white w-screen h-screen"></div>}>
         <Outlet />
       </Suspense>
