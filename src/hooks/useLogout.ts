c703481a51de4472595f8a8e09c42de113208100
poncy <poncyman@gmail.com>
2023-11-30 02:56:03 +0100

    feat:added toast when user logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -2,12 +2,18 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { API_URL } from "../config/config";
 import { useNavigate } from "react-router-dom";
+import { useGlobalDispatch } from "../global/hooks";
+import { setLoading } from "../global/toggle-slice";
+import { toast } from "sonner";
 
 const useLogout = () => {
   const token = Cookies.get("refreshToken");
   const navigate = useNavigate();
+  const dispatch = useGlobalDispatch();
 
   const logout = async () => {
+    dispatch(setLoading({ loading: true }));
+
     try {
       const response = await axios({
         method: "delete",
@@ -20,11 +26,14 @@ const useLogout = () => {
       const data = response.data;
 
       if (data?.logout) {
+        dispatch(setLoading({ loading: false }));
+        toast.success("Successfully logged out");
         Cookies.remove("refreshToken");
         Cookies.remove("accessToken");
         navigate("/");
       }
     } catch (err) {
+      dispatch(setLoading({ loading: false }));
       Cookies.remove("refreshToken");
       Cookies.remove("accessToken");
       navigate("/");
