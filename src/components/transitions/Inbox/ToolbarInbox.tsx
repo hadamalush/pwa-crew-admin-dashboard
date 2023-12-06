33eb102338b3490943e18608af0a85b3430996e5
poncy <poncyman@gmail.com>
2023-12-06 01:32:34 +0100

    feat: option permanent delete messages added and sending query to delete messages - component ToolbarInbox

--- src/components/transitions/Inbox/ToolbarInbox.tsx
+++ src/components/transitions/Inbox/ToolbarInbox.tsx
@@ -16,11 +16,16 @@ import { useParams } from "react-router";
 import { getMessIndex } from "../../../global/message-action";
 import { axiosPrivate } from "../../../api/axios";
 import { toast } from "sonner";
+import { AnimatePresence } from "framer-motion";
+import Modal from "../Modal";
+import { useState } from "react";
+import Heading from "../../UI/Heading";
 
 const ToolbarInbox = () => {
   const dispatch = useGlobalDispatch();
   const toggleState = useGlobalSelector((state) => state.toggle);
   const messagesState = useGlobalSelector((state) => state.messages);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { path } = usePage();
   const { messageId } = useParams();
@@ -72,6 +77,30 @@ const ToolbarInbox = () => {
     dispatch(setLoading({ loading: false }));
   };
 
+  const handleDeleteMessage = async () => {
+    setIsModalOpen(false);
+    dispatch(setLoading({ loading: true }));
+    let res;
+
+    try {
+      res = await axiosPrivate.post("/admin/inbox/deletePermanent", checkedMessages);
+    } catch (err) {
+      dispatch(setLoading({ loading: false }));
+      toast.error("Failed to move messages");
+      return;
+    }
+
+    if (res.data?.error) {
+      dispatch(setLoading({ loading: false }));
+      toast.error(res.data.error);
+      return;
+    } else {
+      toast.success(res.data.message);
+    }
+
+    dispatch(setLoading({ loading: false }));
+  };
+
   const handleChangePage = (action: "previous" | "next") => {
     if (
       (lastIndexMess &&
@@ -85,142 +114,179 @@ const ToolbarInbox = () => {
   };
 
   return (
-    <Container
-      className={cn(
-        "rounded-none bg-slate-50 dark:bg-primaryDarkBrighter h-20 fixed top-28 right-0 md:left-[29rem] left-0 justify-end duration-200 z-30",
-        { "md:left-[44rem]": isVisibleMainNav }
-      )}
-      as="div"
-    >
-      {!messageId && (
-        <>
-          <input
-            type="checkbox"
-            className="cursor-pointer ml-5 mr-2 md:ml-20"
-            onChange={handleMessagesCheckbox}
-            checked={isMarkedCheckboxAll}
-            name="checkboxAll"
-            id="checkboxAll"
-          />
-          <label
-            htmlFor="checkboxAll"
-            className="dark:text-textPrimary w-max text-xl md:text-2xl mr-auto sm:mr-0"
+    <>
+      <AnimatePresence>
+        {isModalOpen && (
+          <Modal
+            onClose={() => setIsModalOpen(false)}
+            className="sm:w-[50rem]  h-full sm:h-auto overflow-y-auto rounded-none sm:rounded-xl"
+            title="Delete messages"
           >
-            Select all
-          </label>
-        </>
-      )}
+            <Heading as="h3" className="text-center py-10 text-lightRed font-bold">
+              Are you sure to delete messages?
+            </Heading>
+            <Container className="flex gap-5">
+              <Button
+                variant="default"
+                size="big"
+                className="mb-20 mt-10 px-14"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="default"
+                size="big"
+                className="mb-20 mt-10 px-14"
+                onClick={() => handleDeleteMessage()}
+              >
+                Confirm
+              </Button>
+            </Container>
+          </Modal>
+        )}
+      </AnimatePresence>
+
       <Container
-        as="div"
         className={cn(
-          "mr-auto absolute left-0 bottom-[-4rem] invisible opacity-0 bg-slate-50 dark:bg-primaryDarkBrighter w-full justify-start h-16 sm:static sm:w-max sm:ml-2 duration-200",
-          { "visible opacity-100": isCheckedMessage }
+          "rounded-none bg-slate-50 dark:bg-primaryDarkBrighter h-20 fixed top-28 right-0 md:left-[29rem] left-0 justify-end duration-200 z-30",
+          { "md:left-[44rem]": isVisibleMainNav }
         )}
+        as="div"
       >
-        {isCheckedMessage && !path.includes("trash") && (
-          <Button
-            variant="outline"
-            className="group pl-3 pr-3 outline-none order-1 md:order-none block"
-            aria-label="Move to trash"
-            type="button"
-            onClick={() => handleMessagesMove("trash")}
-          >
-            <Icon
-              iconName="trash"
-              size="s1_5"
-              color="yellow"
-              className=" group-hover:text-lightBlue duration-200"
+        {!messageId && (
+          <>
+            <input
+              type="checkbox"
+              className="cursor-pointer ml-5 mr-2 md:ml-20"
+              onChange={handleMessagesCheckbox}
+              checked={isMarkedCheckboxAll}
+              name="checkboxAll"
+              id="checkboxAll"
             />
-          </Button>
+            <label
+              htmlFor="checkboxAll"
+              className="dark:text-textPrimary w-max text-xl md:text-2xl mr-auto sm:mr-0"
+            >
+              Select all
+            </label>
+          </>
         )}
-        {isCheckedMessage && !path.includes("spam") && (
-          <Button
-            variant="outline"
-            className="group p-5 pr-3 outline-none order-1 md:order-none block"
-            aria-label="Move to spam"
-            type="button"
-            onClick={() => handleMessagesMove("spam")}
-          >
-            <Icon
-              iconName="flame"
-              size="s1_5"
-              color="green"
-              className=" group-hover:text-lightBlue duration-200"
-            />
-          </Button>
+        <Container
+          as="div"
+          className={cn(
+            "mr-auto absolute left-0 bottom-[-4rem] invisible opacity-0 bg-slate-50 dark:bg-primaryDarkBrighter w-full justify-start h-16 sm:static sm:w-max sm:ml-2 duration-200",
+            { "visible opacity-100": isCheckedMessage }
+          )}
+        >
+          {isCheckedMessage && (
+            <Button
+              variant="outline"
+              className="group pl-3 pr-3 outline-none order-1 md:order-none block"
+              aria-label="Move to trash"
+              type="button"
+              onClick={() => {
+                if (!path.includes("trash")) return handleMessagesMove("trash");
+                setIsModalOpen(true);
+              }}
+            >
+              <Icon
+                iconName="trash"
+                size="s1_5"
+                color="yellow"
+                className=" group-hover:text-lightBlue duration-200"
+              />
+            </Button>
+          )}
+          {isCheckedMessage && !path.includes("spam") && (
+            <Button
+              variant="outline"
+              className="group p-5 pr-3 outline-none order-1 md:order-none block"
+              aria-label="Move to spam"
+              type="button"
+              onClick={() => handleMessagesMove("spam")}
+            >
+              <Icon
+                iconName="flame"
+                size="s1_5"
+                color="green"
+                className=" group-hover:text-lightBlue duration-200"
+              />
+            </Button>
+          )}
+          {isCheckedMessage && path !== "/inbox" && (
+            <Button
+              variant="outline"
+              className="group p-5 pr-3 outline-none order-1 md:order-none block"
+              aria-label="Move to inbox"
+              type="button"
+              onClick={() => handleMessagesMove("inbox")}
+            >
+              <Icon
+                iconName="inbox"
+                size="s1_5"
+                color="purple"
+                className=" group-hover:text-lightBlue duration-200"
+              />
+            </Button>
+          )}
+        </Container>
+
+        {!messageId && quantityMessages !== 0 && (
+          <p className="dark:text-textPrimary mr-5 text-xl">
+            {(firstIndexMess && firstIndexMess + 1) || (!firstIndexMess && 1)} &nbsp;- &nbsp;
+            {lastIndexMess && quantityMessages && lastIndexMess > quantityMessages
+              ? quantityMessages
+              : lastIndexMess}{" "}
+            of {quantityMessages}
+          </p>
         )}
-        {isCheckedMessage && path !== "/inbox" && (
-          <Button
-            variant="outline"
-            className="group p-5 pr-3 outline-none order-1 md:order-none block"
-            aria-label="Move to inbox"
-            type="button"
-            onClick={() => handleMessagesMove("inbox")}
-          >
-            <Icon
-              iconName="inbox"
-              size="s1_5"
-              color="purple"
-              className=" group-hover:text-lightBlue duration-200"
-            />
-          </Button>
+
+        {!messageId && quantityMessages && quantityMessages > 0 && (
+          <>
+            <Button
+              variant="outline"
+              className="group p-2 md:p-5 outline-none order-1 md:order-none"
+              aria-label="Previous page"
+              type="button"
+              onClick={() => handleChangePage("previous")}
+            >
+              <Icon
+                iconName="arrowLeftMini"
+                size="s1_5"
+                color="gray"
+                className="group-hover:text-lightBlue"
+              />
+            </Button>
+
+            <Button
+              variant="outline"
+              className="group p-2 md:p-5 outline-none order-1 md:order-none"
+              aria-label="Next page"
+              type="button"
+              onClick={() => handleChangePage("next")}
+            >
+              <Icon
+                iconName="arrowRightMini"
+                size="s1_5"
+                color="gray"
+                className="group-hover:text-lightBlue"
+              />
+            </Button>
+          </>
         )}
+
+        <Button
+          variant="outline"
+          className="group p-5 pr-3 outline-none order-1 md:order-none md:hidden block"
+          aria-label="Mark as featured"
+          type="button"
+          onClick={handleInboxNavChange}
+        >
+          <Icon iconName="menu" size="s1_5" color="gray" className=" group-hover:text-lightBlue" />
+        </Button>
       </Container>
-
-      {!messageId && quantityMessages !== 0 && (
-        <p className="dark:text-textPrimary mr-5 text-xl">
-          {(firstIndexMess && firstIndexMess + 1) || (!firstIndexMess && 1)} &nbsp;- &nbsp;
-          {lastIndexMess && quantityMessages && lastIndexMess > quantityMessages
-            ? quantityMessages
-            : lastIndexMess}{" "}
-          of {quantityMessages}
-        </p>
-      )}
-
-      {!messageId && quantityMessages && quantityMessages > 0 && (
-        <>
-          <Button
-            variant="outline"
-            className="group p-2 md:p-5 outline-none order-1 md:order-none"
-            aria-label="Previous page"
-            type="button"
-            onClick={() => handleChangePage("previous")}
-          >
-            <Icon
-              iconName="arrowLeftMini"
-              size="s1_5"
-              color="gray"
-              className="group-hover:text-lightBlue"
-            />
-          </Button>
-
-          <Button
-            variant="outline"
-            className="group p-2 md:p-5 outline-none order-1 md:order-none"
-            aria-label="Next page"
-            type="button"
-            onClick={() => handleChangePage("next")}
-          >
-            <Icon
-              iconName="arrowRightMini"
-              size="s1_5"
-              color="gray"
-              className="group-hover:text-lightBlue"
-            />
-          </Button>
-        </>
-      )}
-
-      <Button
-        variant="outline"
-        className="group p-5 pr-3 outline-none order-1 md:order-none md:hidden block"
-        aria-label="Mark as featured"
-        type="button"
-        onClick={handleInboxNavChange}
-      >
-        <Icon iconName="menu" size="s1_5" color="gray" className=" group-hover:text-lightBlue" />
-      </Button>
-    </Container>
+    </>
   );
 };
 
